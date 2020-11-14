9a8f5c7c51eb085ae0479f9b2e303798f75fab06
Øystein Buvik <ohbuvik@gmail.com>
2020-11-14 20:34:25 +0100

    Initial Commit

---
+++ nyGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nyGit
+//
+//  Created by Øystein Hangeland Buvik on 14/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
